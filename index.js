50cea008e0a0ca98c87a2321c8b957b9beacc08c
deepakcit <deepakkumarcsecit@gmail.com>
2023-03-27 09:47:53 +0530

    change

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-
-
 const express= require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const app = express();
 app.use(cors())
-let port =process.env.PORT ||  8080;
+let port =process.env.PORT ||  8000;
 const conn = require("./database/db");
 conn();
 
-const surveylist = require('../backend/routes/surveylist')
-const registerRoute = require('../backend/routes/register')
+const surveylist = require('./routes/surveylist'); // corrected file path
+const registerRoute = require('./routes/register');
 const loginRoute = require("./routes/login");
 const createques = require('./routes/displayQuestions')
-const createSurveyRoute = require("../backend/routes/createSurvey");
-const requestedquestionsroute = require('../backend/routes/reqquestions');
+const createSurveyRoute = require("./routes/createSurvey");
+const requestedquestionsroute = require('./routes/reqquestions');
 const  routerTheme = require("./themes/themes")
 app.use(bodyParser.json());
 app.use(routerTheme)
@@ -22,10 +20,6 @@ app.use(createques)
 app.use(registerRoute);
 app.use(loginRoute);
 app.use(createSurveyRoute);
-app.use(surveylist)
-app.use(requestedquestionsroute)
+app.use(surveylist);
+app.use(requestedquestionsroute);
 app.listen(port, () => console.log(`app running on port ${port}`));
-
-
-
-
